8a17242c8834fbe8d65b2b1ff5e71ef940f5c58b
Shakib <dev@shakib.my.id>
2025-11-10 15:30:31 +0600

    stagging
    Signed-off-by: Shakib <dev@shakib.my.id>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,18 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+ifeq ($(TARGET_DEVICE),rock)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,ba70d784e5fc1b41224b5baf74c225e891a2470e)
+$(call add-radio-file-sha1-checked,radio/gz.img,c3e77147d9d36f649d5878f8b03f498f7b877da1)
+$(call add-radio-file-sha1-checked,radio/lk.img,5c24687b69ffb0ca29ad47b25b1cc0d1fe3a6761)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,1289a02f310138263385d3c94926683b87bf12f3)
+$(call add-radio-file-sha1-checked,radio/md1img.img,517503e92e20f9632c1eed70864af6a017cf49d5)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,c12a659b9b2cf74c30d55553fe4faa2f3b3e91e4)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,7b98750858165a58ebfa65bfe7c01112c5198efd)
+$(call add-radio-file-sha1-checked,radio/scp.img,ec9593b99120ed719d0916955519c0fc755588ac)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,f4aacf2d2e3bdfd35857170a562016901bdc7847)
+$(call add-radio-file-sha1-checked,radio/sspm.img,1cf30234cd224422c3d50978f1155fb1695692e7)
+$(call add-radio-file-sha1-checked,radio/tee.img,f0f11fcf504ac56c9324ce4632277c7d3e7e3213)
+
+endif
